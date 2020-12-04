58886b2c2cf413467834fe35901887ce946d73f0
Bruno Almeida <bdalmeida@live.com>
2020-12-04 18:48:12 -0300

    Adicionando o Swagger-UI

--- src/main/java/com/afrodev/apiAvaliacao/resources/ProdutoResource.java
+++ src/main/java/com/afrodev/apiAvaliacao/resources/ProdutoResource.java
@@ -2,6 +2,8 @@ package com.afrodev.apiAvaliacao.resources;
 
 import com.afrodev.apiAvaliacao.models.Produto;
 import com.afrodev.apiAvaliacao.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,32 +11,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API avaliação AfroDev-Alelo")
+@CrossOrigin(origins = "*")
 public class ProdutoResource {
 
     @Autowired
     ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos")
+    @ApiOperation(value = "Retorna uma lista de produtos")
     public List<Produto> listaProdutos(){
         return produtoRepository.findAll();
     }
 
     @GetMapping("/produtos/{id}")
+    @ApiOperation(value = "Retorna um único produto")
     public Produto listaProdutoUnico(@PathVariable(value="id") long id){
         return produtoRepository.findById(id);
     }
 
     @PostMapping("/produto")
+    @ApiOperation(value = "Este metodo salva um produto")
     public Produto salvaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
 
     @DeleteMapping("/produto")
+    @ApiOperation(value = "Este metodo deleta um produto")
     public void deletaProduto(@RequestBody Produto produto){
         produtoRepository.delete(produto);
     }
 
     @PutMapping("/produto")
+    @ApiOperation(value = "Este metodo atualiza um produto")
     public Produto atualizaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
---
+++ src/test/java/com/afrodev/apiAvaliacao/ApiAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrodev.apiAvaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
